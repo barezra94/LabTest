e9c6e798e2115cafa26a98dbf4d1d1c120cc63f1
Bar Ezra <barezra94@gmail.com>
2020-04-15 16:28:31 +0300

    Update calculate_pca.py
    Added more features to the featuers list

--- python_scripts/calculate_pca.py
+++ python_scripts/calculate_pca.py
@@ -15,11 +15,34 @@ features = {
     "MCHC": "mean_corpuscular_haemoglobin_concentration_f30060_0_0",
     # White Blood Cells
     "WBC": "white_blood_cell_leukocyte_count_f30000_0_0",
+    "LYM": "lymphocyte_count_f30120_0_0",
+    "LYM_precent": "lymphocyte_percentage_f30180_0_0",
+    "MONO": "monocyte_count_f30130_0_0",
+    "MONO_percent": "monocyte_percentage_f30190_0_0",
+    "NEU": "neutrophill_count_f30140_0_0",
+    "EOS_percent": "eosinophill_percentage_f30210_0_0",
+    "BASO_percent": "basophill_percentage_f30220_0_0",
     # Platelet Count
     "PLT": "platelet_count_f30080_0_0",
+    "MPV": "mean_platelet_thrombocyte_volume_f30100_0_0",
+    # Uncatagorized
+    "Albumin_blood": "albumin_f30600_0_0",
+    "Alkaline_Phosphatase_blood": "alkaline_phosphatase_f30610_0_0",
+    "Calcium_total_blood": "calcium_f30680_0_0",
+    "Cholesterol_blood": "cholesterol_f30690_0_0",
+    "Creatinin_blood": "creatinine_f30700_0_0",
+    "GammaGT_blood": "gamma_glutamyltransferase_f30730_0_0",
+    "Glucose_blood": "glucose_f30740_0_0",
+    "HDL_cholesterol_blood": "hdl_cholesterol_f30760_0_0",
+    "LDL_cholesterol_calculated_blood": "ldl_direct_f30780_0_0",
+    "Bilirubin_total_blood": "total_bilirubin_f30840_0_0",
+    "Triglycerides_blood": "triglycerides_f30870_0_0",
 }
 
-# 'RDW': 'mean_corpuscular_volume_f30040_0_0' ??
+all_features = {
+    "w": "creactive_protein_f30710_0_0",  # Is this equal to wide range or high sensitive
+    "ab": "phosphate_f30810_0_0",  # What is this
+}
 
 df_uk = pd.read_csv("../research/blood_test_uk.csv")
 df_uk_added_data = pd.read_csv("../research/ICD10_UK.csv")
